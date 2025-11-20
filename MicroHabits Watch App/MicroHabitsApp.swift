4c82348296a977ae45a4dfc7a56357ab5f87d1ec
Muna Onuorah <jonuorah@ymail.com>
2025-11-20 16:05:46 -0600

    Initial Commit

---
+++ MicroHabits Watch App/MicroHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroHabitsApp.swift
+//  MicroHabits Watch App
+//
+//  Created by Muna Onuorah on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MicroHabits_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
